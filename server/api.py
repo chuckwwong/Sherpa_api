9b6f90f76bc7455e373d5dd41875dcc4a91ce1e0
Chuck Wong <cwwong3@illinois.edu>
2020-04-25 19:17:56 -0500

    server updated, postman json included for testing, cors added to allow communication between UI and backend, fixed file storage in backend

--- server/api.py
+++ server/api.py
@@ -2,9 +2,11 @@
 import os, json
 from src import findFlows, makeEvals, sherpa
 from flask import Flask, request, send_file
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+cors = CORS(app,origins=['http://localhost:*'])
 # Create a directory in a known location to save files to.
 uploads_dir = 'uploads'
 if not os.path.exists(uploads_dir):
@@ -15,10 +17,16 @@ if app.debug:
 
 
 def allowed_file(filename):
+    '''
+    Helper Function to make uploaded filenames secure
+    '''
     return '.' in filename and \
         filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
 def ret_json(success=True,status=200,msg=None,sess=None,flows=None,links=None):
+    '''
+    Helper Function to format json response output
+    '''
     if not flows and not links:
         if msg:
             return json.dumps({'success':success,'message':msg}),status,{'ContentType':'application/json'}
@@ -33,10 +41,18 @@ def upload_config():
     '''
     upload network configuration json files to server
     return flows and links that user can choose to evaluate
-      input:  topology - json
-              rules - json
-              nodeIPs - json
-      output:
+
+    Requst Arguments:
+        name:       name of the session
+        mh:         the minimum number of hops
+    File Arguments:
+        topology:   user json input of network topology
+        rules:      user json input of network rules
+        nodeIPs:    user json input of network IPs
+    output:
+        sess:       name of the created session for identification
+        flows:      the list of flows in this session with given mh
+        links:      the list of links in this session with given mh
     '''
     #### Need to make the operation atomic in case of failure
     #parameter check
@@ -62,6 +78,8 @@ def upload_config():
     os.makedirs(session_n)
     # results folder to store experiment results
     os.makedirs(os.path.join(session_n,'results'))
+    # evaluation folder to store old evaluations to be reused
+    os.makedirs(os.path.join(session_n,'evals'))
 
     # store topology, rules, nodeIPs
     topology = request.files['topology']
@@ -95,10 +113,13 @@ def load_config():
     '''
     load in configuration to use
     return flows and links that user can choose to evaluate
-    run get_flow_rules
-      input:  shell script
-      output: flows - json
-              session - json
+    
+    Request Arguments:
+        sess:       the session folder to be used
+    output:
+        session:    the session name
+        flows:      the list of flows in this session with given mh
+        links:      the list of links in this session with given mh
     '''
     if 'session_name' not in request.args:
         return ret_json(False,404,msg='file name not provided')
@@ -110,12 +131,15 @@ def load_config():
     sess_file = os.path.join(session_n,'session.json')
     linksList, flowsDict = makeEvals.get_flows_rules(sess_file)
     # get session file and run makeEvals, and return the links and flows
-    return ret_json(sess=session_n,flows=flowsDict,links=linksList)
+    return ret_json(sess=sess,flows=flowsDict,links=linksList)
 
 @app.route('/sessions',methods=["GET"])
 def get_sessions():
     '''
     get a list of all the configurations that were saved to file
+
+    output:
+        sessions:   list of all sessions created by the user
     '''
     configs = []
     configs = os.listdir(uploads_dir)
@@ -130,14 +154,18 @@ def run_sherpa():
     '''
     run experiment with evaluation
     from selected links and flows to evaluate, run sherpa on
-    input:
+
+    Request Arguments:
         session_name: the session to pull previously uploaded data from
-        eval_name: name of user specified evaluation
-        flows: array of user selected flows to evaluate
-        links: array of user selected links to evaluate
+        eval_name:    name of user specified evaluation
+    JSON Arguments:
+        flows:        array of user selected flows to evaluate
+        links:        array of user selected links to evaluate
     output:
-        output file: json output of experiment ran on evaluation
+        output file:  json output of experiment ran on evaluation
     '''
+    #### Need to make atomic in case of operation failure
+
     if 'session_name' not in request.args:
         return ret_json(False,404,msg='file name not provided')
     # check if session exists in uploads folder
@@ -152,9 +180,9 @@ def run_sherpa():
         return ret_json(False,404,msg='evaluation name not provided')
     eval_n = request.args['eval_name']
     out_n = eval_n + '_out.json'
-    eval_n = eval_n + '.json'
-    eval_file = os.path.join(session_n,eval_n)
-    out_file = os.path.join(session_n,out_n)
+    eval_n = eval_n + '_eval.json'
+    eval_file = os.path.join(session_n,"evals",eval_n)
+    out_file = os.path.join(session_n,"results",out_n)
 
     # get selected flows and links array
     form_json = request.get_json()
@@ -169,21 +197,52 @@ def run_sherpa():
     return send_file(out_file,as_attachment=True)
 
 
-@app.route('/evals',methods=["POST"])
+@app.route('/evals',methods=["GET"])
 def get_evals():
     '''
     To be made in the next step.
     This Api will get list of all evaluations corresponding to
     this session.
-    '''
-    pass
 
-@app.route('/load_evals',methods=["DELETE"])
+    Request Arguments:
+        session_name: name of the session upload
+    output:
+        evaluations:  list of the evaluations that were previously created
+    '''
+    if 'session_name' not in request.args:
+        return ret_json(False,404,msg='file name not provided')
+    # check if session exists in uploads folder
+    sess = request.args["session_name"]
+    session_n = os.path.join(uploads_dir,sess)
+    if not os.path.exists(session_n):
+        return ret_json(False,404,msg='Session does not exist')
+    eval_fol_p = os.path.join(session_n,'evals')
+    if not os.path.exists(eval_fol_p):
+        return ret_json(False,404,msg='Evalution Folder does not exist')
+
+    evals = []
+    for file in os.listdir(eval_fol_p):
+        # only add evaluation files to return response
+        if file.endswith('_eval.json'):
+            evals.append(file)
+    return json.dumps({'success':True,'evaluations': evals}),200,{'ContentType':'application/json'}
+
+
+@app.route('/load_evals',methods=["GET"])
 def load_eval():
     '''
     To be made in the next step.
     This Api will allow users to load evaluations to the 
     eval file before submitting to run experiment
+    '''
+    pass
+
+@app.route('/rm_eval',methods=["DELETE"])
+def rm_eval():
+    '''
+    To be made in the next step.
+    This API will allow users to remove old evalutions
+    from the eval folder
     '''
     pass
 
